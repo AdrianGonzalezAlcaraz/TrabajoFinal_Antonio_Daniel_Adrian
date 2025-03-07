b0213a3c673c242e45101f98b1d1ffe5b71d15f1
0414dcano <100593419+0414dcano@users.noreply.github.com>
2025-03-07 10:06:58 +0100

    Se han cambiado los if-else por la opcion de or die

--- crear_bd_productos.php
+++ crear_bd_productos.php
@@ -12,20 +12,14 @@ if(!$conexion) {
     //SQL para crear la base de datos productosbd
     $sql = "CREATE DATABASE IF NOT EXISTS productosbd";
     //se crea la base de datos
-    if(mysqli_query($conexion,$sql)){
+    mysqli_query($conexion,$sql) or die("Ocurrio un error al intentar crear la base de datos productosbd");
         echo "Se ha logrado crear la base de datos productosbd <br>";
         //selecionamos la base de datos
         mysqli_select_db($conexion,"productosbd");
         //SQL para crear la tabla productos
         $sql2 = "CREATE TABLE IF NOT EXISTS productos (id_producto INT PRIMARY KEY NOT NULL, nombre VARCHAR(20), descripcion VARCHAR(255), precio INT, fotografia BLOB)";
         //se crea la tabla
-        if(mysqli_query($conexion,$sql2)) {
+        mysqli_query($conexion,$sql2) or die("Ocurrio un error al intentar crear la tabla productos");
             echo "Se a logrado crear la tabla productos <br>";
-        } else {
-            echo "Ocurrio un error al intentar crear la tabla productos";
         }
-    } else {
-        echo "Ocurrio un error al intentar crear la base de datos productosbd";
-    }
-}
 ?>
